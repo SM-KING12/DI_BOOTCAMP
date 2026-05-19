cd45a81538af429a493ded824a4bfe3269f4cc1d
SM_KING12 <smking.1207@gmail.com>
2026-05-19 16:30:55 +0000

    exercises of xp

---
+++ Week1/ExerciceXP/Day1/exercise.py
@@ -0,0 +1,46 @@
+# Exercise1
+print(("Hello World\n")*4)
+#Exercise2
+resultat=((99**3)*8)
+print(resultat)
+#Exercise3
+>>> 5 < 3 #False
+>>> 3 == 3 #True
+>>> 3 == "3" #False
+>>> "3" > 3 #False
+>>> "Hello" == "hello"#False
+#Exercise4
+computer_brand="Lamborgini"
+print("I have a " + computer_brand + "computer .")
+#Exercise5
+name="SORO"
+age= 22
+shoe_size= 43
+info= f"Je suis {name}, j'ai {age} ans et je chausse du {shoe_size}"
+print(info)
+#Exercise6
+a=79
+b=89
+if a > b:
+    print("Hello World")
+ #Exercise7
+ numero=int(input("Veuillez saisir un nombre svp : "))
+if numero %2 ==0 :
+    print("Ce nombre est bien pair")
+else:
+    print("Ce nombre est impair")
+#Exercise8
+nom= input("Quel est votre nom svp : ")
+if nom == "Soro":
+    print("Que vous etes drole !!")
+ #Exercise9
+ taille=float(input("Quelle taille mesurez vous ? : " "en centimètres"))
+if taille>145 :
+    print("Vous etes assez grand pour monter à chéval")
+else:
+    print("Désolé , vous n'etes pas aasez grand pour monter à cheval, il vous faut grandir un peu plus.")
+
+   
+
+
+   
